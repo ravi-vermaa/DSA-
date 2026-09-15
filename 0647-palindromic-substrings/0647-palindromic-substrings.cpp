aba02a42c4875ffa5b3bc37510784aedc39a0902
Ravi <136078768+ravi-vermaa@users.noreply.github.com>
2026-09-16 03:14:22 +0530

    Time: 10 ms (54.79%), Space: 21.4 MB (23.4%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,20 +1,32 @@
+
 class Solution {
 public:
-    int countSubstrings(string s) {
-        int count=0;
-        for(int i=0;i<s.length();i++){
-            string ans="";
-            ans+=s[i];
-            for(int j=i+1;j<s.length();j++){
-                ans+=s[j];
-                string temp=ans;
-                reverse(temp.begin(),temp.end());
-                if(ans==temp)
-                    count++;
-                
-                
-            }
+    int palindrome(string s, int i, int j) {
+        int count = 0;
+
+        while(i >= 0 && j < s.length() && s[i] == s[j]) {
+            count++;
+            i--;
+            j++;
         }
-        return count+s.length();
+
+        return count;
+    }
+
+    int countSubstrings(string s) {
+        int count = 0;
+
+        for(int centre = 0; centre < s.length(); centre++) {
+
+            // Odd-length palindromes
+            int ans1 = palindrome(s, centre, centre);
+
+            // Even-length palindromes
+            int ans2 = palindrome(s, centre, centre + 1);
+
+            count += ans1 + ans2;
+        }
+
+        return count;
     }
 };
